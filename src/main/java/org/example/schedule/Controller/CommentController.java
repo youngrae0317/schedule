cf7b9d24816e0ef8ee8725902082cb3d31ef0bdc
youngrae0317 <isp1229@naver.com>
2025-08-01 17:41:07 +0900

    ResponseEntity 반환 구현

--- src/main/java/org/example/schedule/Controller/CommentController.java
+++ src/main/java/org/example/schedule/Controller/CommentController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.example.schedule.DTO.CommentRequestDto;
 import org.example.schedule.DTO.CommentResponseDto;
 import org.example.schedule.Service.CommentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +19,7 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("/schedules/{id}/comments")
-    public CommentResponseDto createComment(@PathVariable Long id, @RequestBody CommentRequestDto commentRequestDto) {
-        return commentService.createComment(id, commentRequestDto);
+    public ResponseEntity<CommentResponseDto> createComment(@PathVariable Long id, @RequestBody CommentRequestDto commentRequestDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(commentService.createComment(id, commentRequestDto));
     }
 }
